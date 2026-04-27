ed9ee68b865f57d2a0c9c15d7ff2c08704003b0b
BhartiSinghal16 <bhartisinghal763@gmail.com>
2026-04-27 22:58:04 +0530

    Vercel deployment config

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,6 @@ LOGGING = {
         },
     },
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
